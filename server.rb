5ef2eef6f1742745c6cfc02422c97dd6253100ef
Robin Thomas <>
2018-04-05 12:59:05 -0500

    Questions passed from back-end

--- server.rb
+++ server.rb
@@ -1,6 +1,13 @@
 require "sinatra"
 require "sinatra/reloader"
 
+Questions = [
+	"Is the sky blue?",
+	"Do you live in the USA?",
+	"Are you male?",
+	"Are you female?"
+]
+
 get "/" do
 	erb :index
 end
